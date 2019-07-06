fce75c4dae425f92713d1ba82e271645d021ad6b
Rob Paik <robpaik@gmail.com>
2019-07-06 22:33:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,11 @@
 def reverse_each_word(string)
   array = string.split("")
   
+  
+  
+  
+  
+  
+  #convert string to array
+  #recapture array in reverse order
+  #covert reversed array to string and return
